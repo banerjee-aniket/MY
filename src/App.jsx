6730b4ebdb6da08cd02e946f0a46422834f8e1c5
aniketbanerjee12233 <aniketbanerjee12233@gmail.com>
2025-04-23 13:55:47 +0000

    Fix: Fix Feed.jsx file, now the build is successful

--- src/App.jsx
+++ src/App.jsx
@@ -15,15 +15,6 @@ const App = () => {
      const [user, setUser] = useState(null);
     const [isPremium, setIsPremium] = useState(false);
 
-    console.log("VITE_FIREBASE_API_KEY:", import.meta.env.VITE_FIREBASE_API_KEY);
-    console.log("VITE_FIREBASE_AUTH_DOMAIN:", import.meta.env.VITE_FIREBASE_AUTH_DOMAIN);
-    console.log("VITE_FIREBASE_PROJECT_ID:", import.meta.env.VITE_FIREBASE_PROJECT_ID);
-    console.log("VITE_FIREBASE_STORAGE_BUCKET:", import.meta.env.VITE_FIREBASE_STORAGE_BUCKET);
-    console.log("VITE_FIREBASE_MESSAGING_SENDER_ID:", import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID);
-    console.log("VITE_FIREBASE_APP_ID:", import.meta.env.VITE_FIREBASE_APP_ID);
-    console.log("VITE_FIREBASE_MEASUREMENT_ID:", import.meta.env.VITE_FIREBASE_MEASUREMENT_ID);
-
-
 
     useEffect(() => {
         setLoading(true);
@@ -48,7 +39,6 @@ const App = () => {
                 });
 
             }
-            setLoading(false);
         });
 
         return () => unsubscribe();
